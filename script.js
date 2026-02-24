2e4a0dee9800be23750619e40f589f82d460c6a0
Ojas-space <ojas101kumar@gmail.com>
2026-02-24 14:37:52 +0530

    consoleuse

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("hello world this is it");
+document.write("HELLOOOOOOOOOO");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="abc";
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+num=[1,2,3];
+console.log(num);
+console.log(typeof(num));
+var hel=function() {
+    alert('sayhello');
+}
+console.log(hel);
