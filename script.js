acabc3b49047363617780d43c313b9b764e71a3b
Ojas-space <ojas101kumar@gmail.com>
2026-02-24 14:44:33 +0530

    used symbol

--- script.js
+++ script.js
@@ -19,3 +19,10 @@ var hel=function() {
     alert('sayhello');
 }
 console.log(hel);
+
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2) console.log("true");
+else console.log("false");
