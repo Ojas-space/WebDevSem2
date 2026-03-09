b31af7e93a36d4b608fff784782cfa956f75164e
Ojas-space <ojas101kumar@gmail.com>
2026-03-09 09:57:25 +0530

    last

--- script.js
+++ script.js
@@ -26,3 +26,10 @@ const sym2=Symbol(4);
 console.log(sym2);
 if(sym1==sym2) console.log("true");
 else console.log("false");
+
+var a=10;var b=12;var c=14;var linebreak="<br>"
+document.write(linebreak);
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
