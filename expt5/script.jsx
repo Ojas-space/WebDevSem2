351b5833e348b7f58023fede393cc7452fd6a7df
Ojas-space <ojas101kumar@gmail.com>
2026-03-26 17:50:11 +0530

    antigravity

---
+++ expt5/script.jsx
@@ -0,0 +1,6 @@
+let employees = [];
+function addEmpoyee() {
+    let name =document.getElementById("name").value;
+    let empId =document.getElementById("empId").value;
+    let salary =parseFloat();
+}
