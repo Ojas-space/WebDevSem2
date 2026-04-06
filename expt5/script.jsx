69022762e3e2655b56461903071cd4ef5e99e6d0
Ojas-space <ojas101kumar@gmail.com>
2026-04-06 20:22:04 +0530

    changes finalised

--- expt5/script.jsx
+++
@@ -1,6 +0,0 @@
-let employees = [];
-function addEmpoyee() {
-    let name =document.getElementById("name").value;
-    let empId =document.getElementById("empId").value;
-    let salary =parseFloat();
-}
